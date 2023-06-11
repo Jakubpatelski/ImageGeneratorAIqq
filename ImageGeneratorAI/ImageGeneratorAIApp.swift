b83f4a601bfeddcba4239045d0a258b798f0b3c7
Jakubpatelski <jakubpatelski24@gmail.com>
2023-06-11 13:18:02 +0200

    Initial Commit

---
+++ ImageGeneratorAI/ImageGeneratorAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorAIApp.swift
+//  ImageGeneratorAI
+//
+//  Created by Jakub Patelski on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
